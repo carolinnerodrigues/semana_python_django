01d54c336c7d03b15d20658d0f1991fb6155dcc0
Carolinne Santos Rodrigues <carolinne.rodrigues7@hotmail.com>
2020-07-12 23:45:41 -0300

    Add bootstrap

--- gerenciador_tarefas/app/views.py
+++ gerenciador_tarefas/app/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def listar_tarefas(request):
-    return render(request, 'tarefas/listar_tarefas.html')
+    nome_tarefa = "Assistir a Semana Python e Django da TreinaWeb"
+    return render(request, 'tarefas/listar_tarefas.html', {"nome_tarefa": nome_tarefa})
